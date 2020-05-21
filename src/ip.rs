e09ac31754bd47da3f742699d3cddc6dd243ad0e
Emamul Andalib <emamul.andalib@gmail.com>
2020-05-21 17:59:53 +0600

    version 0.0.1

---
+++ src/ip.rs
@@ -0,0 +1,48 @@
+use std::net::{IpAddr};
+
+type IpMaskType = [u8; 4];
+
+static CLASS_A_MASK: IpMaskType = [0xff, 0, 0, 0];
+static CLASS_B_MASK: IpMaskType = [0xff, 0xff, 0, 0];
+static CLASS_C_MASK: IpMaskType = [0xff, 0xff, 0xff, 0];
+
+pub trait IpMask {
+    fn default_mask(&self) -> Option<IpMaskType>;
+}
+
+impl IpMask for IpAddr {
+    fn default_mask(&self) -> Option<IpMaskType> {
+        match to_4(self) {
+            Some(octet) => {
+                match octet[0] {
+                    0x80 => Some(CLASS_A_MASK),
+                    0xC0 => Some(CLASS_B_MASK),
+                    _ => Some(CLASS_C_MASK)
+                }
+            }
+            None => None
+        }
+    }
+}
+
+pub(crate) fn is_zeros(ip: &[u8]) -> bool {
+    ip.iter().all(|&x| x == 0)
+}
+
+pub(crate) fn to_4(ip: &IpAddr) -> Option<[u8; 4]> {
+    if let IpAddr::V4(ipv4) = ip {
+        return Some(ipv4.octets());
+    }
+
+    if let IpAddr::V6(ipv46) = ip {
+        let mut octets = ipv46.octets();
+
+        if is_zeros(&octets) && &octets[10] == &0xff && &octets[11] == &0xff {
+            let v: [u8; 4] = [0, 0, 0, 0];
+            octets[12..16].clone_from_slice(&v);
+            return Some(v);
+        }
+    }
+
+    None
+}
